c4e7260a3b1a4707e55cf25625b0ef9ee6d08410
stolschm <oliver.schmidtke@student.hs-anhalt.de>
2023-01-17 16:31:49 +0100

    Added support for custom RML rules and directly provided JSON data

--- app/src/main/kotlin/question/answering/dataset/rdfizer/App.kt
+++ app/src/main/kotlin/question/answering/dataset/rdfizer/App.kt
@@ -33,69 +33,98 @@ class JSONNotFoundException(url: String): Throwable() {
     override val message = "$url not found"
 }
 
+class MissingCustomRMLException: Throwable() {
+    override val message = "RML rules required for mode \"custom\""
+}
+
 
 class MappingNotDoneException(override val message: String): Throwable()
 
+class MissingJsonDataException(override val message: String): Throwable()
+
 
 @Serializable
-data class Json2RDFTransformer(private var filePath: String, private var format: String,
-                          private val label: String, private val homepage: String) {
+data class Json2RDFTransformer(private var filePath: String? = null, private var format: String, private val json: String? = null,
+                          private val label: String, private val homepage: String, private val rml: String? = null) {
     fun mapRDF(): String {
         val ontology = File("ontology/qa-benchmark-ontology.ttl").readText()
 
         format = format.uppercase().replace("..", "").replace("/", "")
         format = format.replace("\\", "")
 
+        if(format == "CUSTOM" && rml == null) {
+            throw MissingCustomRMLException()
+        }
+
         // create temporary
         val tempFile: File = kotlin.io.path.createTempFile().toFile()
         val tempRMLRules: File = kotlin.io.path.createTempFile().toFile()
 
-        val outputText: String
         var jsonObject: String
 
         // fetch JSON file
-        do {
-            val client: HttpClient = HttpClient.newBuilder().build()
-            val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(filePath)).build()
-            val response: HttpResponse<*> = client.send(request, HttpResponse.BodyHandlers.ofString())
-            val contentType = response.headers().allValues("content-type")[0]
+        if (filePath != null) {
+            do {
+                val client: HttpClient = HttpClient.newBuilder().build()
+                val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(filePath!!)).build()
+                val response: HttpResponse<*> = client.send(request, HttpResponse.BodyHandlers.ofString())
+                val contentType = response.headers().allValues("content-type")[0]
 
-            if(response.statusCode() >= 400) {
-                throw JSONNotFoundException(filePath)
-            }
-
-            jsonObject = response.body().toString()
-
-            if(response.statusCode() in 300..399) {
-                filePath = response.headers().allValues("location")[0]
-
-                if(filePath.startsWith("/")) {
-                    filePath = "${request.uri().toURL().protocol}://${request.uri().host}$filePath"
+                if(response.statusCode() >= 400) {
+                    throw JSONNotFoundException(filePath!!)
                 }
 
-                continue
-            }
+                jsonObject = response.body().toString()
 
-            if(response.statusCode() == 200 && contentType != "application/json" &&
-                !contentType.contains("text/plain") && !contentType.contains("application/binary")) {
-                throw JSONNotFoundException(filePath)
+                if(response.statusCode() in 300..399) {
+                    filePath = response.headers().allValues("location")[0]
+
+                    if(filePath!!.startsWith("/")) {
+                        filePath = "${request.uri().toURL().protocol}://${request.uri().host}$filePath"
+                    }
+
+                    continue
+                }
+
+                if(response.statusCode() == 200 && contentType != "application/json" &&
+                    !contentType.contains("text/plain") && !contentType.contains("application/binary")) {
+                    throw JSONNotFoundException(filePath!!)
+                }
+            } while (response.statusCode() != 200)
+        }
+        else {
+            if (json != null) {
+                jsonObject = json
             }
-        } while (response.statusCode() != 200)
+            else {
+                throw MissingJsonDataException("Please provide a \"filePath\" or a \"json\"!")
+            }
+        }
 
         try {
             // store JSON data
             tempFile.writeText(jsonObject)
 
-            // select mapping file
-            val mapPath = "mappings/${format}.ttl"
-            val mappingFile = File(mapPath)
+            var newMapping = if(format != "CUSTOM") {
+                // select mapping file
+                val mapPath = "mappings/${format}.ttl"
+                val mappingFile = File(mapPath)
 
-            // read mapping file and replace placeholder
-            var newMapping = mappingFile.readText()
+                // read mapping file and replace placeholder
+                mappingFile.readText()
+            } else {
+                rml!!
+            }
+
             newMapping = newMapping.replace("example.json", tempFile.absolutePath)
-            newMapping = newMapping.replace("URL", filePath.replace("#", "%23"))
             newMapping = newMapping.replace("LABEL", label.replace(' ', '-'))
             newMapping = newMapping.replace("HOMEPAGE", homepage)
+
+            newMapping = if(filePath != null) {
+                newMapping.replace("URL", filePath!!.replace("#", "%23"))
+            } else {
+                newMapping.replace("URL", "urn:local:file")
+            }
 
             tempRMLRules.writeText(newMapping)
 
@@ -124,9 +153,11 @@ data class Json2RDFTransformer(private var filePath: String, private var format:
 
             result?.write(out, "turtle")
 
-            outputText = output.toString()
+            val outputText = output.toString()
 
             out.close()
+
+            return outputText
         } catch (e: Exception) {
             e.printStackTrace()
             throw MappingNotDoneException(e.message?: "Unknown error occurred")
@@ -135,35 +166,45 @@ data class Json2RDFTransformer(private var filePath: String, private var format:
             tempFile.delete()
             tempRMLRules.delete()
         }
-
-        return outputText
     }
 }
 
 
 fun main() {
-    embeddedServer(Netty, port = 8080) {
-        install(ContentNegotiation) {
-            json()
-        }
+    embeddedServer(Netty, port = 8080, module = Application::rdfizerApplication).start(wait = true)
+}
 
-        routing {
-            post("/json2rdf") {
-                val json2rdfTransformer = call.receive<Json2RDFTransformer>()
-                try {
-                    call.respondText(json2rdfTransformer.mapRDF(), ContentType("application",
-                        "n-triples"))
-                } catch (e: JSONNotFoundException) {
-                    call.respondText(e.message, ContentType.Text.Plain, HttpStatusCode.BadRequest)
-                } catch (e: MappingNotDoneException) {
-                    call.respondText(e.message, ContentType.Text.Plain, HttpStatusCode.InternalServerError)
-                }
-            }
-            get("/json2rdf") {
-                call.respondText("The RDFizer service is running. Please view the documentation at " +
-                        "<a href=\"https://github.com/anbo-de/question-answering-dataset-RDFizer#readme\">Github</a>.",
-                ContentType.Text.Html)
+fun Application.rdfizerApplication() {
+    install(ContentNegotiation) {
+        json()
+    }
+
+    routing {
+        post("/json2rdf") {
+            val json2rdfTransformer = call.receive<Json2RDFTransformer>()
+            try {
+                call.respondText(
+                    json2rdfTransformer.mapRDF(), ContentType(
+                        "application",
+                        "n-triples"
+                    )
+                )
+            } catch (e: JSONNotFoundException) {
+                call.respondText(e.message, ContentType.Text.Plain, HttpStatusCode.BadRequest)
+            } catch (e: MappingNotDoneException) {
+                call.respondText(e.message, ContentType.Text.Plain, HttpStatusCode.InternalServerError)
+            } catch (e: MissingCustomRMLException) {
+                call.respondText(e.message, ContentType.Text.Plain, HttpStatusCode.BadRequest)
+            } catch (e: MissingJsonDataException) {
+                call.respondText(e.message, ContentType.Text.Plain, HttpStatusCode.BadRequest)
             }
         }
-    }.start(wait = true)
+        get("/json2rdf") {
+            call.respondText(
+                "The RDFizer service is running. Please view the documentation at " +
+                        "<a href=\"https://github.com/anbo-de/question-answering-dataset-RDFizer#readme\">Github</a>.",
+                ContentType.Text.Html
+            )
+        }
+    }
 }
