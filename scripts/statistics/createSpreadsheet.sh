643c6a5abfc2e79e1ed51861714cfce902931ffa
oliver <oscgm98@gmail.com>
2023-05-13 10:05:22 +0200

    Statistics generation without docker build cache

--- scripts/statistics/createSpreadsheet.sh
+++ scripts/statistics/createSpreadsheet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t sparql-spreadsheet-creator:latest .
+docker build --no-cache -t sparql-spreadsheet-creator:latest .
 mkdir ~/QADO-statistics > /dev/null 2>&1
 docker run --rm -it -v ~/QADO-statistics/:/output/ sparql-spreadsheet-creator:latest
